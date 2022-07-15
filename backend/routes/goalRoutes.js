1fc087f3eb82f8c41cf1b2bd2585526bb1757afc
ahmadseptianto <191240000983@unisnu.ac.id>
2022-07-15 21:49:07 +0700

    Protect Goal Routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,10 @@
-const { 
-    getGoals,
-    setGoal,
-    updateGoal,
-    deleteGoal
- } = require('../controllers/goalController')
+getGoals, 
+setGoal, 
+updateGoal, 
+deleteGoal } = require('../controllers/goalController')
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+
+const {protect} = require('../middleware/authMiddleware')
+
+router.route('/').get( protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal) 
