3d9ce79588c99febca095ba753f059f0169ae003
ahmadseptianto <191240000983@unisnu.ac.id>
2022-07-15 21:58:43 +0700

    Setting a User Goal

--- backend/Controllers/goalController.js
+++ backend/Controllers/goalController.js
@@ -1,13 +1,7 @@
-const getGoals = asyncHandler (async(req, res)=>{
-    const goals = await Goal.find({ user: req.user.id })
-    const setGoal = asyncHandler (async(req, res)=>{
-        const goal = await Goal.create({
-            text: req.body.text,
-            user: req.user.id, 
-        })
-        // @route   PUT /api/goals
-        const updateGoal =asyncHandler (async(req, res)=>{
-            // @route   delete /api/goals
-            const deleteGoal = asyncHandler (async(req, res)=>{
-            })
-            getGoals,  
+throw new Error ('Goal not found')
+}
+
+
+
+
+await goal.remove()
