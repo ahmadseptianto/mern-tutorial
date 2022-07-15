8c773d83fc0e49e76e79ba9264f40e1f2498425f
ahmadseptianto <191240000983@unisnu.ac.id>
2022-07-15 21:54:27 +0700

    Get Only Users Goals

--- backend/Controllers/goalController.js
+++ backend/Controllers/goalController.js
@@ -1,9 +1,13 @@
-const asyncHandler = require('express-async-handler')
-const getGoals = asyncHandler(async(req,res) => {
-})
-const setGoal = asyncHandler(async(req,res) => {
-})
-const updateGoal = asyncHandler(async(req,res) => {
-})
-const deleteGoal = asyncHandler(async(req,res) => {
-})
+const getGoals = asyncHandler (async(req, res)=>{
+    const goals = await Goal.find({ user: req.user.id })
+    const setGoal = asyncHandler (async(req, res)=>{
+        const goal = await Goal.create({
+            text: req.body.text,
+            user: req.user.id, 
+        })
+        // @route   PUT /api/goals
+        const updateGoal =asyncHandler (async(req, res)=>{
+            // @route   delete /api/goals
+            const deleteGoal = asyncHandler (async(req, res)=>{
+            })
+            getGoals,  
